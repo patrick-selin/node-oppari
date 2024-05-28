760dd0c4db3bc05f934d6a188d01e782c0392962
Patrick Selin <patrick.selin@gmail.com>
2024-05-28 15:19:08 +0300

    buffers.ts

---
+++ buffers.ts
@@ -0,0 +1,6 @@
+// Importing the 'Buffer' class from the 'buffer' module
+import { Buffer } from 'buffer';
+
+// Creating a buffer with a specified size (in bytes)
+const bufSize: number = 10;
+const buffer: Buffer = Buffer.alloc(bufSize);
