8ccee029e0d5a1d87e02c7823db241c0ef50cef4
Patrick Selin <patrick.selin@gmail.com>
2024-05-28 15:27:40 +0300

    buffers.ts b

--- buffers.ts
+++ buffers.ts
@@ -1,6 +1,14 @@
 // Importing the 'Buffer' class from the 'buffer' module
-import { Buffer } from 'buffer';
+import { Buffer } from "buffer";
 
 // Creating a buffer with a specified size (in bytes)
 const bufSize: number = 10;
 const buffer: Buffer = Buffer.alloc(bufSize);
+
+console.log(buffer); // <Buffer 00 00 00 00 00 00 00 00 00 00>
+console.log(buffer[0]); // 0
+
+buffer[0] = 0xf4; // allocate 244 (hex) to buffer element 0
+
+console.log(buffer); // <Buffer f4 00 00 00 00 00 00 00 00 00>
+console.log(buffer[0]); // 244
