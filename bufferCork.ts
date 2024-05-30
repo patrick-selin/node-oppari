e6517f618fb2103aa03832445a0fb5620423bd12
Patrick Selin <patrick.selin@gmail.com>
2024-05-30 13:06:10 +0300

    fs readfile

--- bufferCork.ts
+++ bufferCork.ts
@@ -1,10 +1,10 @@
 import { createWriteStream } from "fs";
 const writeStream = createWriteStream("output.txt");
-writeStream.write("Hello\n");
+writeStream.write("Helloo\n");
 writeStream.cork();
-writeStream.write("World\n");
+writeStream.write("Worlds\n");
 setTimeout(() => {
   writeStream.uncork();
-  writeStream.write("!");
+  writeStream.write("!!");
   writeStream.end();
 }, 5000);
