5075e6631ee638ccf685a73fb1e9b70240b043d1
Patrick Selin <patrick.selin@gmail.com>
2024-05-30 13:21:34 +0300

    fs write

--- fileSystem.ts
+++ fileSystem.ts
@@ -1,5 +1,22 @@
-const fs = require("fs")
+import * as fs from "fs";
 
-const content = fs.readFileSync("./text.txt");
+// Reading a file asynchronously
+fs.readFile("example.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log("File content:", data);
+});
 
-console.log(content.toString("utf-8"));
+// Reading a file synchronously
+const dataSync = fs.readFileSync("example.txt", "utf8");
+console.log("Synchronous file content:", dataSync);
+
+//
+// Writing to a file asynchronously
+fs.writeFile("output.txt", "Hello, Node.js!", (err) => {
+  if (err) throw err;
+  console.log("File has been written");
+});
+
+// Writing to a file synchronously
+fs.writeFileSync("outputSync.txt", "Hello, Node.js!");
+console.log("Synchronous file write complete");
