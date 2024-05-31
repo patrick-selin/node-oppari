c51aeb6b9f74d398220a4a7e6528b172946be87c
Patrick Selin <patrick.selin@gmail.com>
2024-05-31 15:28:15 +0300

    streams b

--- streams1.ts
+++ streams1.ts
@@ -13,3 +13,25 @@ readStream.on("data", (chunk) => {
 readStream.on("end", () => {
   console.log("Finished reading file");
 });
+
+//
+// Writing to a file using a writable stream
+const writeStream = fs.createWriteStream("output.txt");
+
+// Writing data to the stream
+writeStream.write("Hello, ");
+writeStream.write("Node.js!");
+writeStream.end(() => {
+  console.log("Finished writing to file");
+});
+
+//
+// Piping data from a readable stream to a writable stream
+const inputStream = fs.createReadStream("example.txt");
+const outputStream = fs.createWriteStream("pipedOutput.txt");
+inputStream.pipe(outputStream);
+
+// Handling 'finish' event
+outputStream.on("finish", () => {
+  console.log("Finished piping data");
+});
