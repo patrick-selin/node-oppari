78d4bf4980014ac537b4011cfdc64579f7a68e43
Patrick Selin <patrick.selin@gmail.com>
2024-06-03 14:01:37 +0300

    streams g

--- streams1.ts
+++ streams1.ts
@@ -73,23 +73,23 @@ const fs = require("node:fs/promises");
 (async () => {
   console.time("readBig");
   const fileHandleRead = await fs.open("test.txt", "r");
-  // const fileHandleWrite = await fs.open("dest.txt", "w");
+  const fileHandleWrite = await fs.open("dest.txt", "w");
 
   const streamRead = fileHandleRead.createReadStream({
     highWaterMark: 64 * 1024,
   });
 
-  // const streamWrite = fileHandleWrite.createWriteStream();
+  const streamWrite = fileHandleWrite.createWriteStream();
 
   streamRead.on("data", (chunk: Buffer) => {
-    // if (!streamWrite.write(chunk)) {
-    //   streamRead.pause();
-    // }
+    if (!streamWrite.write(chunk)) {
+      streamRead.pause();
+    }
     console.log(chunk)
     console.log(chunk.length)
   });
 
-  // streamWrite.on("drain", () => {
-  //   streamRead.resume();
-  // });
+  streamWrite.on("drain", () => {
+    streamRead.resume();
+  });
 })();
