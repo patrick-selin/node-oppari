e8ace08df0a97102e66ae0415116b2ffd0629d9f
Patrick Selin <patrick.selin@gmail.com>
2024-06-04 15:43:42 +0300

    streams2e

--- streams2.ts
+++ streams2.ts
@@ -1,24 +1,29 @@
 (async () => {
   console.time("copy");
 
-  const srcFile = await fs.open("text-gigantic.txt", "r");
-  const destFile = await fs.open("text-copy.txt", "w");
+  const srcFile = await fs.open("testc.txt", "r");
+  const destFile = await fs.open("dest.txt", "w");
 
-  let bytesRead = -1;
+  const readStream = srcFile.createReadStream();
+  const writeStream = destFile. crateWriteStream();
 
-  while (bytesRead !== 0) {
-    const readResult = await srcFile.read();
-    bytesRead = readResult.bytesRead;
+  readStream.pipe(writeStream;)
 
-    if (bytesRead !== readResult.buffer.length) {
-      const indexOfNotFilled = readResult.buffer.indexOf(0);
-      const newBuffer = Buffer.alloc(indexOfNotFilled);
-      readResult.buffer.copy(newBuffer, 0, 0, indexOfNotFilled);
-      destFile.write(newBuffer);
-    } else {
-      destFile.write(readResult.buffer);
-    }
-  }
+//   let bytesRead = -1;
 
-  console.timeEnd("copy");
+//   while (bytesRead !== 0) {
+//     const readResult = await srcFile.read();
+//     bytesRead = readResult.bytesRead;
+
+//     if (bytesRead !== readResult.buffer.length) {
+//       const indexOfNotFilled = readResult.buffer.indexOf(0);
+//       const newBuffer = Buffer.alloc(indexOfNotFilled);
+//       readResult.buffer.copy(newBuffer, 0, 0, indexOfNotFilled);
+//       destFile.write(newBuffer);
+//     } else {
+//       destFile.write(readResult.buffer);
+//     }
+//   }
+
+//   console.timeEnd("copy");
 })();
