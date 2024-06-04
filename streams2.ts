ad8242aae85d56225d48d160b293ca88e99f09dd
Patrick Selin <patrick.selin@gmail.com>
2024-06-04 15:38:40 +0300

    streams2d

--- streams2.ts
+++ streams2.ts
@@ -9,6 +9,15 @@
   while (bytesRead !== 0) {
     const readResult = await srcFile.read();
     bytesRead = readResult.bytesRead;
+
+    if (bytesRead !== readResult.buffer.length) {
+      const indexOfNotFilled = readResult.buffer.indexOf(0);
+      const newBuffer = Buffer.alloc(indexOfNotFilled);
+      readResult.buffer.copy(newBuffer, 0, 0, indexOfNotFilled);
+      destFile.write(newBuffer);
+    } else {
+      destFile.write(readResult.buffer);
+    }
   }
 
   console.timeEnd("copy");
