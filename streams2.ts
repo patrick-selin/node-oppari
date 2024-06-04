3741b6a35c77f3fd91bceb0ba9e30363e9e8fc91
Patrick Selin <patrick.selin@gmail.com>
2024-06-04 15:21:04 +0300

    streams2b

--- streams2.ts
+++ streams2.ts
@@ -1,13 +1,8 @@
-const fs = require("node:fs/promises");
-
 (async () => {
   console.time("copy");
-  const destFile = await fs.open("test.txt", "w");
-  const result = await fs.readFile("text1.txt");
 
-//   await destFile.write(result);
+  const srcFile = await fs.open("text-gigantic.txt", "r");
+  const destFile = await fs.open("text-copy.txt", "w");
 
-  console.timeEnd("copy");
+
 })();
-
-
