5781f390dd1e2faa2974e13e58fe66cdc542b87d
Patrick Selin <patrick.selin@gmail.com>
2024-06-04 15:21:46 +0300

    streams2c

--- streams2.ts
+++ streams2.ts
@@ -4,5 +4,12 @@
   const srcFile = await fs.open("text-gigantic.txt", "r");
   const destFile = await fs.open("text-copy.txt", "w");
 
+  let bytesRead = -1;
 
+  while (bytesRead !== 0) {
+    const readResult = await srcFile.read();
+    bytesRead = readResult.bytesRead;
+  }
+
+  console.timeEnd("copy");
 })();
