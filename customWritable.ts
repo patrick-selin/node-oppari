acff1deafc520cdf0358a8bd002c74d92276d920
Patrick Selin <patrick.selin@gmail.com>
2024-06-05 15:52:06 +0300

    custom writable4

--- customWritable.ts
+++ customWritable.ts
@@ -1,36 +1,43 @@
 const { Writable } = require("node:stream");
+const fs = require("node:fs")
 
 class FileWriteStream extends Writable {
-  constructor({ highWaterMark, fileName }) {
-    super({ highWaterMark });
+    constructor({ highWaterMark, fileName }: { highWaterMark: number; fileName: string }) {
+        super({ highWaterMark });
+
     this.fileName = fileName;
     this.fd = null;
+    this.chunks = [];
+    this.chunksSize = 0;
   }
 
-  _construct(callback) {
-    fs.open(this.fileName, 'w', (err, fd) => {
-        if (err) {
-            callback(err)
-        } else {
-            this.fd = fd;
-        }
-    })
-
-    callback();
+  _construct(callback: (error?: Error) => void) {
+    fs.open(this.fileName, "w", (err, fd) => {
+      if (err) {
+        callback(err);
+      } else {
+        this.fd = fd;
+        callback();
+      }
+    });
   }
 
   _write(chunk, encoding, callback) {
-    fs.write(this.fd, chunk, callback);
+    // fs.write(this.fd, chunk, callback);
+    this.chunks.push(chunk)
+    this.chunksSize += chunk.length;
 
- 
+    if (this.chunnkSize)
   }
 }
 
-const stream = new FileWriteStream({ highWaterMark: 1800 });
+const stream = new FileWriteStream({
+  highWaterMark: 1800,
+  fileName: "test.txt",
+});
 stream.write(Buffer.from("this is some string"));
 stream.end(Buffer.from("Our last write."));
 
-
 stream.on("drain", () => {
-    // todo
+  // todo
 });
