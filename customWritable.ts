3bcce6f11214f9ca48b7f133fcdc3a3f438b21b8
Patrick Selin <patrick.selin@gmail.com>
2024-06-05 15:30:14 +0300

    custom writable2

--- customWritable.ts
+++ customWritable.ts
@@ -1,8 +1,16 @@
 const { Writable } = require("node:stream");
 
 class FileWriteStream extends Writable {
-  constructor({ highWaterMark, filename }) {
+  constructor({ highWaterMark, fileName }) {
     super({ highWaterMark });
     this.fileName = fileName;
   }
+
+  _write(chuk, encoding, callback) {
+   // todo 
+  }
 }
+
+const stream = new FileWriteStream({ highWaterMark: 1800 });
+stream.write(Buffer.from("this is some string"));
+stream.end(Buffer.from("Our last write."));
